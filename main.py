d0349428633c65fc37db42d6de0b73e73ff79dad
ali-hajhashemi <jimmy.hajhashemi@gmail.com>
2025-01-05 21:13:57 +0330

    Add check result function to while loop

--- main.py
+++ main.py
@@ -1,30 +1,34 @@
 # Import area
 import random
 
+# Variable area
+items = ['rock','paper','scissors']
 
 # Function area
 def check_result(user,system):
     if user == 'rock' and system == 'rock':
-        print('Try again')
+        print('Try again, system answer:', system)
     elif user == 'rock' and system == 'paper':
-        print('You lose')
+        print('You lose, system answer:', system)
     elif user == 'rock' and system == 'scissors':
-        print('You win')
+        print('You win, system answer:', system)
     elif user == 'paper' and system == 'paper':
-        print('Try again')
+        print('Try again, system answer:', system)
     elif user == 'paper' and system == 'rock':
-        print('You win')
+        print('You win, system answer:', system)
     elif user == 'paper' and system == 'scissors':
-        print('You lose')
+        print('You lose, system answer:', system)
     elif user == 'scissors' and system == 'scissors':
-        print('Try again')
+        print('Try again, system answer:', system)
     elif user == 'scissors' and system == 'rock':
-        print('You lose')
+        print('You lose, system answer:', system)
     elif user == 'scissors' and system == 'paper':
-        print('You win')
+        print('You win, system answer:', system)
     
 
 # Main area
 print('Welcome to rock, paper, scissors game')
 while True:
-    user_answer = input("Please write your choice: ")
+    user_answer = input("\n\nPlease write your choice: ")
+    system_answer = random.choice(items)
+    check_result(user_answer,system_answer)
